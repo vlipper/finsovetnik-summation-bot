b2a531e46e5bc74cb1c75cdf4f76650193fcce4c
vlipper <ign.legeza@gmail.com>
2025-10-26 17:55:29 +0200

    Update article mining interval

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,7 @@
 import os
 
 # general
-ARTICLE_MINING_INTERVAL = 60 * 1  # 5 min
+ARTICLE_MINING_INTERVAL = 60 * 5  # 5 min
 CATCH_UP_ARTICLES = 1
 DATABASE_PATH = "./db/prod.sqlite"
 BOT_TOKEN = os.environ["BOT_TOKEN"]
