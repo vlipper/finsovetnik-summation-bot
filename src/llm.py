20e921d8af145aa017a625c898900fc7d5211aea
vlipper <30463876+vlipper@users.noreply.github.com>
2026-06-02 20:31:40 +0300

    (#10) Replace gpt with gemini

--- src/llm.py
+++ src/llm.py
@@ -8,7 +8,7 @@ async def query_summary(text: str) -> str:
         {"role": "system", "content": SYSTEM_MESSAGE},
         {"role": "user", "content": text},
     ]
-    response = await acompletion(MODEL_NAME, messages)
+    response = await acompletion(MODEL_NAME, messages, reasoning_effort="none")
     response_content = response["choices"][0]["message"]["content"]
 
     return response_content
