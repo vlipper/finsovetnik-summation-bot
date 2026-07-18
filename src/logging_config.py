435b001cabf98ba8954ab89c5943a9218064d05a
vlipper <ign.legeza@gmail.com>
2026-07-18 22:44:48 +0300

    Clean handlers of non-root loggers

--- src/logging_config.py
+++ src/logging_config.py
@@ -22,3 +22,11 @@ def setup_logging(level: int = logging.INFO) -> None:
     handler = logging.StreamHandler()
     handler.setFormatter(JsonFormatter())
     logging.basicConfig(level=level, handlers=[handler])
+
+    module_logger = logging.getLogger(__name__)
+    root = logging.getLogger()
+    for lg in root.manager.loggerDict.values():
+        if isinstance(lg, logging.Logger) and lg is not root and lg.handlers:
+            module_logger.debug(f"Clearing handlers for logger: {lg.name}")
+            lg.handlers.clear()
+            # logger.propagate = True  # TODO: in case of some loggers not propagating
